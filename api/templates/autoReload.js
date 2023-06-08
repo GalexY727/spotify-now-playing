05b2d5cfd32100be781a0aa6b6c300e2cab6b446
Alexander Hamilton <a.hamilton72006@gmail.com>
2023-06-08 00:06:36 -0700

    Just make a call every second instead of going through the hoops of time
    API rate limit should be 3 calls/sec with a rolling 30s window,
    so there are "no downsides"

--- api/templates/autoReload.js
+++ api/templates/autoReload.js
@@ -9,18 +9,18 @@ function eventLoop(svg) {
     .then(response => response.text())
     .then(html =>  {
         svg.innerHTML = html;
-        fetch("/api/spotify/time", {
-            method: "GET"
-        })
-        .then(response => response.text())
-        .then(time => {
-            time = parseInt(time) + 1000;
-            console.log("Time left in song: " + time/1000 + " seconds");
-            // if time is NaN set it to 600000
-            if (isNaN(time)) {
-                time = 600000;
-            }
-            setTimeout(eventLoop, time, svg);
-        });
+        // fetch("/api/spotify/time", {
+        //     method: "GET"
+        // })
+        // .then(response => response.text())
+        // .then(time => {
+        //     time = parseInt(time) + 3000;
+        //     console.log("Time left in song: " + time/1000 + " seconds");
+        //     // if time is NaN set it to 600000
+        //     if (isNaN(time)) {
+        //         time = 600000;
+        //     }
+        // });
+        setTimeout(eventLoop, 1000, svg);
     });
 }
